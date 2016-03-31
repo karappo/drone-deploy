c6b7740f46058a65a7540c87260f7ccb89f7a32e
Naokazu Terada <naokazu.terada@gmail.com>
2016-03-31 14:28:58 +0900

    add chmod

--- deploy.sh
+++ deploy.sh
@@ -183,8 +183,11 @@ fi
 if [ "${DEP_INCLUDE_FILE:+isexists}" = "isexists" ]; then
   if [ "${DEP_INCLUDE_FILE:0:7}" = "http://" -o "${DEP_INCLUDE_FILE:0:8}" = "https://" ]; then
     log "| Downloading include file..."
-    wget -O $PWD/.depinc.sh $DEP_INCLUDE_FILE
+    wget -O .depinc.sh $DEP_INCLUDE_FILE
     DEP_INCLUDE_FILE=$PWD/.depinc.sh
+    ls -la
+    chmod +x $DEP_INCLUDE_FILE
+    ls -la
   fi
 fi
 
