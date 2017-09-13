015f233acc6fa22e9ca01e7f8bb34849fb5a23f5
Naokazu Terada <naokazu.terada@gmail.com>
2017-09-13 17:21:53 +0900

    use '--rsh=' istead of '-e' option

--- deploy.sh
+++ deploy.sh
@@ -61,7 +61,7 @@ do_sync()
 
       opt_port=''
       if [ "${DEP_PORT:+isexists}" = "isexists" ]; then
-        opt_port="-e ssh -p $DEP_PORT"
+        opt_port="--rsh='ssh -p $DEP_PORT'"
       fi
       echo '--------'
       echo "rsync -aIzhv --stats --delete $opt_exclude$opt_port . $DEP_USER@$DEP_HOST:$DEP_HOST_DIR"
