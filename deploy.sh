85c5934489b328e2b406e60583fe473947b4db34
Naokazu Terada <naokazu.terada@gmail.com>
2017-09-13 16:53:44 +0900

    Add opt_exclude

--- deploy.sh
+++ deploy.sh
@@ -64,7 +64,7 @@ do_sync()
         opt_port="-e ssh -p $DEP_PORT"
       fi
 
-      if rsync -aIzhv --stats --delete "$opt_port" . "$DEP_USER@$DEP_HOST:$DEP_HOST_DIR"; then
+      if rsync -aIzhv --stats --delete "$opt_port $opt_exclude" . "$DEP_USER@$DEP_HOST:$DEP_HOST_DIR"; then
         log "- sync -> done."
       else
         log "- sync -> [ERROR]"
