71e1d1a85668d0990ef1c08886a9b9bbbc3f432f
Naokazu Terada <naokazu.terada@gmail.com>
2017-09-13 16:56:43 +0900

    for debug

--- deploy.sh
+++ deploy.sh
@@ -63,6 +63,9 @@ do_sync()
       if [ "${DEP_PORT:+isexists}" = "isexists" ]; then
         opt_port="-e ssh -p $DEP_PORT"
       fi
+      echo '--------'
+      echo "rsync -aIzhv --stats --delete $opt_port $opt_exclude . $DEP_USER@$DEP_HOST:$DEP_HOST_DIR"
+      echo '--------'
 
       if rsync -aIzhv --stats --delete "$opt_port $opt_exclude" . "$DEP_USER@$DEP_HOST:$DEP_HOST_DIR"; then
         log "- sync -> done."
