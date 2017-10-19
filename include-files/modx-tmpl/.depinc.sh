7be951ab1c45de94bd80a207017d5cd5afd999c3
Naokazu Terada <naokazu.terada@gmail.com>
2017-10-20 02:26:06 +0900

    WIP: Translate comments

--- include-files/modx-tmpl/.depinc.sh
+++ include-files/modx-tmpl/.depinc.sh
@@ -5,7 +5,7 @@
 # 不用意にテンプレートファイルをそのまま実行しないようにしている
 # [TODO] TODOタグの箇所を書き換えたら下記のを削除
 # ----------------------------------------------------------------
-echo "[DEPLOY] - ERROR -> Template file has included !!"
+echo "[DEPLOY] - ERROR -> Template file has included! You must check all section indicated by [TODO] labels at first."
 exit 1
 # ----------------------------------------------------------------
 
@@ -15,7 +15,7 @@ before_sync(){
   ext=".temp_bakup"
 
   # [TODO]
-  # # 不要なファイルの消去
+  # remove unnecessary files
   rm -rf ./core/cache/*
 
   # remove "DEP_XXX_RM "
@@ -33,7 +33,7 @@ before_sync(){
   sudo find . -name "*.php" -exec sed -i$ext "s|localhost|$DEP_HOST|g" {} \;
 
   # [TODO]
-  # （任意）
+  # OPTIONAL
   # リモートのDBサーバーへのアクセスが「localhost」のみの場合はもう一度元に戻しておく
   # sudo find . -name "*config.inc.php" -exec sed -i".temp_bakup" "s|database_server = '$DEP_HOST';|database_server = 'localhost';|" {} \;
   # sudo find . -name "*config.inc.php" -exec sed -i".temp_bakup" "s|database_dsn = 'mysql:host=$DEP_HOST;|database_dsn = 'mysql:host=localhost;|" {} \;
